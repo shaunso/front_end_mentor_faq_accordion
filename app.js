a22e8064063a7ec48be147f1fbb81762268700cf
shaunso <shaunsom@gmail.com>
2024-05-15 22:57:29 +0200

    Second commit
    Changes made to [app.js], [style.css], [index.html]

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
+// using query selectors to 
 const faq1El = document.querySelector("#faq-1>.faq-header");
 const faq2El = document.querySelector("#faq-2>.faq-header");
 const faq3El = document.querySelector("#faq-3>.faq-header");
 const faq4El = document.querySelector("#faq-4>.faq-header");
-
-const text1EL = document.querySelector("#faq-1>.faq-answer");
-const text2EL = document.querySelector("#faq-2>.faq-answer");
-const text3EL = document.querySelector("#faq-3>.faq-answer");
-const text4EL = document.querySelector("#faq-4>.faq-answer");
 
 faq1El.addEventListener("click", () => {
   document.querySelector("#faq-1").classList.toggle("expand");
@@ -19,11 +15,6 @@ faq1El.addEventListener("click", () => {
   document.querySelector("#faq-3>.faq-answer").classList.remove("visible");
   document.querySelector("#faq-4>.faq-header>.icon").classList.remove("other");
   document.querySelector("#faq-4>.faq-answer").classList.remove("visible");
-
-  text1EL.addEventListener("click", () => {
-  document.querySelector("#faq-1>.faq-header>.icon").classList.remove("other");
-  document.querySelector("#faq-1>.faq-answer").classList.remove("visible");
-  })
 })
 
 faq2El.addEventListener("click", () => {
@@ -38,11 +29,6 @@ faq2El.addEventListener("click", () => {
   document.querySelector("#faq-3>.faq-answer").classList.remove("visible");
   document.querySelector("#faq-4>.faq-header>.icon").classList.remove("other");
   document.querySelector("#faq-4>.faq-answer").classList.remove("visible");
-
-  text2EL.addEventListener("click", () => {
-  document.querySelector("#faq-2>.faq-header>.icon").classList.remove("other");
-  document.querySelector("#faq-2>.faq-answer").classList.remove("visible");
-})
 })
 
 faq3El.addEventListener("click", () => {
@@ -56,11 +42,6 @@ faq3El.addEventListener("click", () => {
   document.querySelector("#faq-2>.faq-answer").classList.remove("visible");
   document.querySelector("#faq-4>.faq-header>.icon").classList.remove("other");
   document.querySelector("#faq-4>.faq-answer").classList.remove("visible");
-
-  text3EL.addEventListener("click", () => {
-  document.querySelector("#faq-3>.faq-header>.icon").classList.remove("other");
-  document.querySelector("#faq-3>.faq-answer").classList.remove("visible");
-  })
 })
 
 faq4El.addEventListener("click", () => {
@@ -74,9 +55,4 @@ faq4El.addEventListener("click", () => {
   document.querySelector("#faq-2>.faq-answer").classList.remove("visible");
   document.querySelector("#faq-3>.faq-header>.icon").classList.remove("other");
   document.querySelector("#faq-3>.faq-answer").classList.remove("visible");
-
-  text4EL.addEventListener("click", () => {
-  document.querySelector("#faq-4>.faq-header>.icon").classList.remove("other");
-  document.querySelector("#faq-4>.faq-answer").classList.remove("visible");
-  })
 })
