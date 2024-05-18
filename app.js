da7dc9c6e0021a4d1eac45f44faf7a879eaf3923
shaunso <shaunsom@gmail.com>
2024-05-18 02:13:37 +0200

    Final commit
    Adding link to google fonts to CSS file & added the default state for the first question to be open in the JS file

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const faq2El = document.querySelector("#faq-2>.faq-header");
 const faq3El = document.querySelector("#faq-3>.faq-header");
 const faq4El = document.querySelector("#faq-4>.faq-header");
 
+// setting the default state of the page as per the challenge
+document.querySelector("#faq-1").classList.add("expand");
+document.querySelector("#faq-1>.faq-header>.icon").classList.add("other");
+document.querySelector("#faq-1>.faq-answer").classList.add("visible");
+
 // adding an event to each query selector that expands and collapses each question the user clicks on it
 faq1El.addEventListener("click", () => {
   document.querySelector("#faq-1").classList.toggle("expand");
